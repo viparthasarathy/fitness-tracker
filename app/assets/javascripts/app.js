06fe49ca341dfa5b25239b4755594c7a77e22851
viparthasarathy <viparthasarathy@gmail.com>
2016-07-03 10:51:25 -0700

    add template for sign in, sign up, and AuthenticationController

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,1 +1,17 @@
-angular.module('fitnessTracker', ['ui.router', 'templates']);
+angular
+  .module('fitnessTracker', ['ui.router', 'templates', 'Devise']);
+    .config(function($stateProvider, $urlRouteProvider) {
+      $stateProvider
+        .state('signup', {
+          url: '/signup',
+          templateUrl: 'auth/_signup.html',
+          controller: 'AuthenticationController as AuthCtrl'
+        })
+        .state('signin', {
+          url: '/signin',
+          templateUrl: 'auth/_signin.html',
+          controller: 'AuthenticationController as AuthCtrl'
+        });
+
+        $urlRouterProvider.otherwise('/top');
+      });
