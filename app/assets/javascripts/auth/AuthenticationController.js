2e92b9a1bdaf129bdffb690f1add529662eb9159
viparthasarathy <viparthasarathy@gmail.com>
2016-07-03 11:41:17 -0700

    Create sign in page

--- app/assets/javascripts/auth/AuthenticationController.js
+++ app/assets/javascripts/auth/AuthenticationController.js
@@ -1,5 +1,15 @@
 function AuthenticationController($scope, $state, Auth) {
+  $scope.login = function() {
+    Auth.login($scope.user).then(function() {
+      console.log("Signed in!")
+    })
+  }
 
+  $scope.register = function() {
+    Auth.register($scope.user).then(Function() {
+      console.log("Signed up!")
+    });
+  }
 }
 
 angular
