1e84de7a345d6cfe58bef0cc43e63b319de0e11a
viparthasarathy <viparthasarathy@gmail.com>
2016-07-03 11:34:07 -0700

    Add logout, signed in, and watchers for navbar

--- app/assets/javascripts/nav/NavigationController.js
+++ app/assets/javascripts/nav/NavigationController.js
@@ -1,5 +1,21 @@
 function NavigationController($scope, Auth) {
+  $scope.signedIn = Auth.isAuthenticated;
+  $scope.logout = Auth.logout;
+  Auth.currentUser().then(function (user) {
+    $scope.user = user;
+  })
 
+  $scope.$on('devise:new-registration', function(event, user) {
+    $scope.user = user;
+  });
+
+  $scope.$on('devise:login', function(event, user) {
+    $scope.user = user;
+  });
+
+  $scope.$on('devise:logout', function(event, user) {
+    $scope.user = {};
+  });
 }
 
 angular
