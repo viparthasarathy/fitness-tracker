dd23e17c0b2352b34c304c7325c2684ac5411bad
viparthasarathy <viparthasarathy@gmail.com>
2016-07-05 13:18:07 -0700

    Begin working on error rendering for authentication

--- app/assets/javascripts/auth/AuthenticationController.js
+++ app/assets/javascripts/auth/AuthenticationController.js
@@ -2,13 +2,17 @@ function AuthenticationController($scope, $state, Auth) {
   $scope.login = function() {
     Auth.login($scope.user).then(function() {
       $state.go('user')
+    }, function(error) {
+      console.log(error);
     })
   }
 
   $scope.register = function() {
     Auth.register($scope.user).then(function() {
       $state.go('user')
-    });
+    }, function(error) {
+      console.log(error);
+    })
   }
 }
 
