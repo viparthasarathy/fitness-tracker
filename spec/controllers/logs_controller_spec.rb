46897c469e01a47d7fd686e5351fdeba5ca96934
viparthasarathy <viparthasarathy@gmail.com>
2016-07-07 19:50:07 -0700

    Add DeviseTest helpers for controller and modify LogsController specs

--- spec/controllers/logs_controller_spec.rb
+++ spec/controllers/logs_controller_spec.rb
@@ -1,26 +1,28 @@
 require 'rails_helper'
 
-describe LogsController do
+describe LogsController, :type => :controller do
 
   describe 'GET #show' do
     context 'logged in' do
       before do
         @user = FactoryGirl.create(:user)
-        login_as(@user, :scope => :user)
-        get :show, :format => :json
-        log_response = JSON.parse(response.body, symbolize_names: true)
+        sign_in @user
       end
 
       it 'is a successful request' do
-        expect(response).to eq(200)
+        get :show, :format => :json
+        expect(response.status).to eq(200)
       end
 
       it 'returns the user log' do
+        get :show, :format => :json
+        log_response = JSON.parse(response.body, symbolize_names: true)
         expect(log_response[:id]).to eq(@user.log.id)
       end
 
       it 'contains references to the log chapters' do
         get :show, :format => :json
+        log_response = JSON.parse(response.body, symbolize_names: true)
         expect(log_response[:chapters][0][:id]).to eq(@user.log.chapters.first.id)
       end
     end
