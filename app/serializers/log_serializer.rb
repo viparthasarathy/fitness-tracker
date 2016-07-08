6e6f4fc2290590e94dc7be45bc50917954057783
viparthasarathy <viparthasarathy@gmail.com>
2016-07-08 12:45:01 -0700

    Add LogSerializer::ChapterSerializer

--- app/serializers/log_serializer.rb
+++ app/serializers/log_serializer.rb
@@ -1,3 +1,8 @@
 class LogSerializer < ActiveModel::Serializer
   attributes :id
+  has_many :chapters
+  class ChapterSerializer < ActiveModel::Serializer
+    attributes :id, :created_at, :completed_at, :title
+  end
+
 end
