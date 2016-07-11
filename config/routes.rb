eba258ca87cc12f9cacc9ccffa45a46447a96c96
viparthasarathy <viparthasarathy@gmail.com>
2016-07-11 15:37:43 -0700

    Add chapters#show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {sessions: 'sessions'}
   resource :log, only: [:show]
-  resources :chapters, only: [:create]
+  resources :chapters, only: [:create, :show]
   root 'application#angular'
 
 end
