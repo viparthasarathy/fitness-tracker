68dc2d01ff1d18c2c61097d8290a2ff3bb024c69
viparthasarathy <viparthasarathy@gmail.com>
2016-07-11 15:29:33 -0700

    Complete get #SHOW specs for ChaptersController

--- spec/controllers/chapters_controller_spec.rb
+++ spec/controllers/chapters_controller_spec.rb
@@ -48,13 +48,16 @@ describe ChaptersController, :type => :controller do
   end
 
   describe 'get #SHOW' do
+    before do
+      @user = FactoryGirl.create(:user)
+      @other_user = FactoryGirl.create(:user, email: "hacker@yahoo.com")
+      @chapter = FactoryGirl.create(:chapter, log: @user.log)
+    end
     context 'logged in' do
-      before do
-        @user = FactoryGirl.create(:user)
-        @chapter = FactoryGirl.create(:chapter, log: @user.log)
-        sign_in @user
-      end
       context 'belongs to the user' do
+        before do
+          sign_in @user
+        end
         it 'is successful' do
           get :show, {:format => :json, :id => @chapter.id}
           expect(response.status).to eq(200)
@@ -64,14 +67,27 @@ describe ChaptersController, :type => :controller do
           get :show, {:format => :json, :id => @chapter.id}
           chapter_response = JSON.parse(response.body, symbolize_names: true)
           expect(chapter_response[:id]).to eq(@chapter.id)
+        end
+
         it 'contains references to the chapter entries'
       end
+
       context 'does not belong to the user' do
-        it 'raises an authorization error'
+        before do
+          sign_in @other_user
+        end
+        it 'raises an authorization error' do
+          get :show, {:format => :json, :id => @chapter.id}
+          expect(response.status).to eq(403)
+        end
       end
     end
+
     context 'logged out' do
-      it 'raises an authentication error'
+      it 'raises an authentication error' do
+        get :show, {:format => :json, :id => @chapter.id}
+        expect(response.status).to eq(401)
+      end
     end
   end
 
