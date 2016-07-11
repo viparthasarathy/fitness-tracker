b5474c407ff0c8c7bab6ddd6c3ae919703ca3b29
viparthasarathy <viparthasarathy@gmail.com>
2016-07-11 12:22:19 -0700

    Begin working on passing ChaptersController specs

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -1,9 +1,10 @@
 class ChaptersController < ApplicationController
   before_action :authenticate_user!
+  before_action :authorize_creation!, only: [:create]
 
   def create
     @chapter = user_log.chapters.build(chapter_params)
-    @chapter.save ? (render json: @chapter, status: 201 : status: 403)
+    @chapter.save ? (render json: @chapter, status: 201) : (render json: @chapter.errors, status: 400)
   end
 
   private
@@ -12,8 +13,8 @@ class ChaptersController < ApplicationController
     params.require(:chapter).permit(:title, :description, :goal)
   end
 
-  def authorize_creation
-    user_log.latest_chapter.completed_at == nil
+  def authorize_creation!
+    render nothing: true, status: 401 if user_log.latest_chapter.completed_at != nil
   end
 
 end
