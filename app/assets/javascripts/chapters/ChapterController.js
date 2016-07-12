365e640d545de29493f525a78971cf8374c6eae0
viparthasarathy <viparthasarathy@gmail.com>
2016-07-12 10:11:45 -0700

    Complete chapters/_show.html for current existing information

--- app/assets/javascripts/chapters/ChapterController.js
+++ app/assets/javascripts/chapters/ChapterController.js
@@ -1,7 +1,11 @@
 function ChapterController(chapterJSON, $state) {
   var ChapterCtrl = this;
   ChapterCtrl.chapter = chapterJSON.data;
-  
+  ChapterCtrl.chapter.created_at = new Date(ChapterCtrl.chapter.created_at)
+  if (ChapterCtrl.chapter.completed_at !== null) { ChapterCtrl.chapter.completed_at = new Date(ChapterCtrl.chapter.completed_at) }
+
+  console.log(ChapterCtrl.chapter)
+
 }
 
 angular
