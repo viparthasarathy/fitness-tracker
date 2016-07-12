41388b2197de74cb01d608914883cec385bdda37
viparthasarathy <viparthasarathy@gmail.com>
2016-07-12 10:56:02 -0700

    Add .markComplete function to ChapterCtrl and create confirmation button on chapters/_show.html

--- app/assets/javascripts/chapters/ChapterController.js
+++ app/assets/javascripts/chapters/ChapterController.js
@@ -1,6 +1,13 @@
-function ChapterController(chapterJSON, $state) {
+function ChapterController(chapterJSON, $state, chapterService) {
   var ChapterCtrl = this;
   ChapterCtrl.chapter = chapterJSON.data;
+  ChapterCtrl.markComplete = function() {
+    chapterService.updateChapter(ChapterCtrl.chapter.id).then(function(response) {
+      $state.go('log')
+    }, function(error) {
+      console.log(error);
+    })
+  }
   ChapterCtrl.chapter.created_at = new Date(ChapterCtrl.chapter.created_at);
   if (ChapterCtrl.chapter.completed_at !== null) {
     ChapterCtrl.chapter.completed_at = new Date(ChapterCtrl.chapter.completed_at);
