b1ebeba790f9514f00192c89395b9350f7c09a54
viparthasarathy <viparthasarathy@gmail.com>
2016-07-13 14:34:39 -0700

    Add column time_zone to Users and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160708193456) do
+ActiveRecord::Schema.define(version: 20160713213351) do
 
   create_table "chapters", force: :cascade do |t|
     t.float    "goal"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 20160708193456) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "time_zone"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
