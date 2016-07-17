c4fc0b3e75920a95901b077572ed5e4f5286591e
viparthasarathy <viparthasarathy@gmail.com>
2016-07-17 10:55:04 -0700

    Fix indentation issues

--- app/models/concerns/totals.rb
+++ app/models/concerns/totals.rb
@@ -1,20 +1,20 @@
 module Totals
   extend ActiveSupport::Concern
 
-    def total_carbs
+  def total_carbs
       self.foods.sum(:carbs)
-    end
+  end
 
-    def total_fats
+  def total_fats
       self.foods.sum(:fats)
-    end
+  end
 
-    def total_protein
+  def total_protein
       self.foods.sum(:protein)
-    end
+  end
 
-    def total_calories
+  def total_calories
       self.foods.sum(:calories)
-    end
+  end
 
 end
