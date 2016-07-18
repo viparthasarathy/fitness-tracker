d962165a166bc4e38abae4c0d2d9f09dc5ddd0d4
viparthasarathy <viparthasarathy@gmail.com>
2016-07-18 14:10:19 -0700

    Work on new Entry form and controller method
    Add form with ng-submit and inputs, add createEntry to ChapterCtrl

--- spec/controllers/entries_controller_spec.rb
+++ spec/controllers/entries_controller_spec.rb
@@ -5,7 +5,7 @@ describe EntriesController, :type => :controller do
     @user = FactoryGirl.create(:user)
     @other_user = FactoryGirl.create(:user, email: "hacker@yahoo.com")
     @chapter = FactoryGirl.create(:chapter, log: @user.log)
-    @entry_params = {day: Time.zone.today, notes: "These are some notes"}
+    @entry_params = {chapter_id: @chapter.id, day: Time.zone.today, notes: "These are some notes"}
   end
 
   describe 'POST #create' do
@@ -14,30 +14,36 @@ describe EntriesController, :type => :controller do
         sign_in @user
       end
 
-      it 'should fail if sent invalid data and return the error' do
-        @entry_params[:day] = Time.zone.today + 1
-        post :create, {:format => :json, :chapter_id => @chapter.id, :entry => @entry_params}
-        expect(response.status).to eq(400)
-        error_response = JSON.parse(response.body, symbolize_names: true)
-        expect(error_response[:messages][:day]).to eq("cannot come after current day")
-      end
+      context 'as owner of chapter'
+        it 'should fail if sent invalid data and return the error' do
+          @entry_params[:day] = Time.zone.today + 1
+          post :create, {:format => :json, :entry => @entry_params}
+          expect(response.status).to eq(400)
+          error_response = JSON.parse(response.body, symbolize_names: true)
+          expect(error_response[:messages][:day]).to eq("cannot come after current day")
+        end
 
-      it 'succeeds if sent proper data' do
-        post :create, {:format => :json, :chapter_id => @chapter.id, :entry => @entry_params}
-        expect(response.status).to eq(201)
-        expect(@chapter.entries.count).to eq(1)
-      end
+        it 'succeeds if sent proper data' do
+          post :create, {:format => :json, :entry => @entry_params}
+          expect(response.status).to eq(201)
+          expect(@chapter.entries.count).to eq(1)
+        end
 
-      it 'returns the entry info on success' do
-        post :create, {:format => :json, :chapter_id => @chapter.id, :entry => @entry_params}
-        entry_response = JSON.parse(response.body, symbolize_names: true)
-        expect(chapter_response[:notes).to eq("These are some notes")
+        it 'returns the entry info on success' do
+          post :create, {:format => :json, :entry => @entry_params}
+          entry_response = JSON.parse(response.body, symbolize_names: true)
+          expect(chapter_response[:notes).to eq("These are some notes")
+        end
       end
+    end
+
+    context 'as other user' do
+      it 'raises an authorization error'
     end
 
     context 'logged out' do
       it 'raises an authentication error' do
-        post :create, {:format => :json, :chapter_id => @chapter.id, :entry => @entry_params}
+        post :create, {:format => :json, :entry => @entry_params}
         expect(response.status).to eq(401)
         expect(@chapter.entries.count).to eq(0)
       end
