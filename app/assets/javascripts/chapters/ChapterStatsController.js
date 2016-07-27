6d6bf2c487c2bc4eef4bc2f064a7e52fdd3a45b3
viparthasarathy <viparthasarathy@gmail.com>
2016-07-27 15:21:02 -0700

    Update chapters/_stats.html to display first measurement information

--- app/assets/javascripts/chapters/ChapterStatsController.js
+++ app/assets/javascripts/chapters/ChapterStatsController.js
@@ -1,15 +1,18 @@
 function ChapterStatsController(chapterJSON) {
   var ChapterStatsCtrl = this;
   ChapterStatsCtrl.chapter = chapterJSON.data;
+  console.log(ChapterStatsCtrl.chapter.measurements);
 
   ChapterStatsCtrl.calculateAverage = function(total) {
-
     if (ChapterStatsCtrl.chapter.entries.length > 0) {
       return Math.round(total / ChapterStatsCtrl.chapter.entries.length)
     } else {
       return 0;
     }
   }
+
+  ChapterStatsCtrl.firstMeasurement = ChapterStatsCtrl.chapter.measurements[ChapterStatsCtrl.chapter.measurements.length - 1];
+  console.log(ChapterStatsCtrl.firstMeasurement);
 }
 
 angular
--- app/serializers/chapter_serializer.rb
+++ app/serializers/chapter_serializer.rb
@@ -1,7 +1,6 @@
 class ChapterSerializer < ActiveModel::Serializer
   attributes :id, :created_at, :completed_at, :title, :description, :goal, :total_calories, :total_carbs, :total_fats, :total_protein
   has_many :entries
-
-
+  has_many :measurements
 
 end
