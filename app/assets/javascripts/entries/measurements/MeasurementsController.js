d9894df6d2bfc01cead2b7f4536215b581d99df2
viparthasarathy <viparthasarathy@gmail.com>
2016-07-26 17:45:18 -0700

    Add newMeasurement form to measurements/_show.html

--- app/assets/javascripts/entries/measurements/MeasurementsController.js
+++ app/assets/javascripts/entries/measurements/MeasurementsController.js
@@ -1,5 +1,7 @@
 function MeasurementsController(EntryJSON, MeasurementService) {
-
+  var MeasurementsCtrl = this;
+  MeasurementsCtrl.entry = EntryJSON.data;
+  MeasurementsCtrl.measurement = MeasurementsCtrl.entry.measurement;
 }
 
 angular
