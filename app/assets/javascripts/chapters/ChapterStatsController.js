20518090993ffc7bc2a151b4f2ea014450601470
viparthasarathy <viparthasarathy@gmail.com>
2016-07-27 15:27:19 -0700

    Add link on chapters/_stats.html to first measurement

--- app/assets/javascripts/chapters/ChapterStatsController.js
+++ app/assets/javascripts/chapters/ChapterStatsController.js
@@ -1,7 +1,7 @@
 function ChapterStatsController(chapterJSON) {
   var ChapterStatsCtrl = this;
   ChapterStatsCtrl.chapter = chapterJSON.data;
-  console.log(ChapterStatsCtrl.chapter.measurements);
+  console.log(ChapterStatsCtrl.chapter);
 
   ChapterStatsCtrl.calculateAverage = function(total) {
     if (ChapterStatsCtrl.chapter.entries.length > 0) {
@@ -10,9 +10,9 @@ function ChapterStatsController(chapterJSON) {
       return 0;
     }
   }
-
+  ChapterStatsCtrl.firstEntryID = ChapterStatsCtrl.chapter.entries[ChapterStatsCtrl.chapter.entries.length - 1].id;
   ChapterStatsCtrl.firstMeasurement = ChapterStatsCtrl.chapter.measurements[ChapterStatsCtrl.chapter.measurements.length - 1];
-  console.log(ChapterStatsCtrl.firstMeasurement);
+
 }
 
 angular
