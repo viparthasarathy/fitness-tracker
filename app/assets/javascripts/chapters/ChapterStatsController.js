fbcc291077595b0454858f70ebf8e418f563d2ba
viparthasarathy <viparthasarathy@gmail.com>
2016-07-28 13:43:24 -0700

    Add .filterWeeklyEntries function to ChapterStatsCtrl

--- app/assets/javascripts/chapters/ChapterStatsController.js
+++ app/assets/javascripts/chapters/ChapterStatsController.js
@@ -57,7 +57,29 @@ function ChapterStatsController(chapterJSON) {
     }
   }
 
-  console.log(ChapterStatsCtrl.chapter.entries);
+  function filterWeeklyEntires(startOfWeek, entries) {
+    var endOfWeek = new Date();
+    endOfWeek.setDate(startOfWeek.getDate() + 7);
+    var weeksEntries = []
+    for (i = 0; i < entries.length; i++) {
+      var entryDay = new Date(entries[i].day);
+      if (entryDay > startOfWeek && entryDay <= endOfWeek) {
+        weeksEntries.push(entries[i]);
+      }
+    }
+    return weeksEntries;
+  }
+
+  var startOfThisWeek = new Date();
+  startOfThisWeek.setDate(startOfThisWeek.getDate() - 7)
+  var startOfLastWeek = new Date();
+  startOfLastWeek.setDate(startOfLastWeek.getDate() - 14)
+
+  var thisWeeksEntries = filterWeeklyEntries(startOfThisWeek, ChapterStatsCtrl.chapter.entries.slice(0, 7));
+  var lastWeeksEntries = filterWeeklyEntries(startOfLastWeek, ChapterStatsCtrl.chapter.entries.slice(0, 14));
+
+  console.log(thisWeeksEntries);
+  console.log(lastWeeksEntries);
 
   ChapterStatsCtrl.firstEntryID = ChapterStatsCtrl.chapter.entries[ChapterStatsCtrl.chapter.entries.length - 1].id;
   ChapterStatsCtrl.firstMeasurement = ChapterStatsCtrl.chapter.measurements[ChapterStatsCtrl.chapter.measurements.length - 1];
