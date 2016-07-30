7b575bd8806d2d7203ad5e4e31a952a7240ecab7
viparthasarathy <viparthasarathy@gmail.com>
2016-07-30 16:11:49 -0700

    Massively reduce ActiveRecord calculation time by removing redundant info
    Previously computing sum of foods in each entry on log/_show.html

--- app/serializers/entry_serializer.rb
+++ app/serializers/entry_serializer.rb
@@ -1,5 +1,5 @@
 class EntrySerializer < ActiveModel::Serializer
-  attributes :id, :day, :notes, :total_calories, :total_fats, :total_protein, :total_carbs
+  attributes :id, :day, :notes
   has_one :measurement
   has_many :foods
 end
