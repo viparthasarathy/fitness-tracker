87c510c456d26fd92827cdaecf14798973b11490
viparthasarathy <viparthasarathy@gmail.com>
2016-08-02 16:06:07 -0700

    Add comment re: pending changes

--- app/serializers/chapter_serializer.rb
+++ app/serializers/chapter_serializer.rb
@@ -3,6 +3,8 @@ class ChapterSerializer < ActiveModel::Serializer
   has_many :entries
   has_many :measurements
 
+  # Look into creating a different ChapterSerializer that's only used in the Angular chapter.stats state.
+  
   def last_weeks_measurements
     last_weeks_entries.map(&:measurement)
   end
