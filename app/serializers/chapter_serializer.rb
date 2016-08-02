2ac6e7a49c0610a8149a6ddccf1d28f8df1e6f0b
viparthasarathy <viparthasarathy@gmail.com>
2016-08-02 15:05:43 -0700

    Implement this_weeks_entries attribute to ChapterSerializer

--- app/serializers/chapter_serializer.rb
+++ app/serializers/chapter_serializer.rb
@@ -1,6 +1,14 @@
 class ChapterSerializer < ActiveModel::Serializer
-  attributes :id, :created_at, :completed_at, :title, :description, :goal, :total_calories, :total_carbs, :total_fats, :total_protein
+  attributes :id, :created_at, :completed_at, :title, :description, :goal, :total_calories, :total_carbs, :total_fats, :total_protein, :last_weeks_entries, :this_weeks_entries
   has_many :entries
   has_many :measurements
+
+  def last_weeks_entries
+
+  end
+
+  def this_weeks_entries
+    object.entries.where(1.week.ago..Date.today)
+  end
 
 end
