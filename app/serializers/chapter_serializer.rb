8c3c032c07c71248a73699fbcad0e61ed2a1aa71
viparthasarathy <viparthasarathy@gmail.com>
2016-08-04 10:00:08 -0700

    Complete reducing page load time by updating JSON serializers

--- app/serializers/chapter_serializer.rb
+++ app/serializers/chapter_serializer.rb
@@ -1,4 +1,4 @@
 class ChapterSerializer < ActiveModel::Serializer
-  attributes :id, :created_at, :completed_at, :title, :description, :goal
+  attributes :id, :created_at, :completed_at, :title, :description, :goal, :total_calories, :total_carbs, :total_fats, :total_protein
   has_many :entries
 end
